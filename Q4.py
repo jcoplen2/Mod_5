7c27724b9e6ebfc3f6d24289e3de2aa7d97fe2ab
jessi <jcoplen2@ivytech.edu>
2025-09-28 13:41:41 -0400

    question four answered

---
+++ Q4.py
@@ -0,0 +1,13 @@
+# import hashlib
+
+# def hash_password(password):
+#     return hashlib.sha1(password.encode()).hexdigest()
+
+##corrected code
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+def verify_password(password: str, hashed: bytes) -> bool:
+    return bcrypt.checkpw(password.encode(), hashed)
